2a3a954167f8e5e1a1c46c294b4e48a0b1d92fb4
Christopher van Dal <cvandal@internode.on.net>
2017-05-12 21:22:37 +1000

    fixed epoch to datetime conversion

--- Nomad/Extensions/DateTimeExtension.cs
+++ Nomad/Extensions/DateTimeExtension.cs
@@ -7,6 +7,6 @@ namespace Nomad.Extensions
         private static readonly DateTime Epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
 
         public static DateTime FromUnixTime(this long unixTime)
-            => Epoch.AddTicks(unixTime / 100).ToLocalTime();
+            => Epoch.AddTicks(Convert.ToInt64(unixTime) / (TimeSpan.TicksPerMillisecond / 100)).ToLocalTime();
     }
 }
