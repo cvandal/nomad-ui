1d78e187c556da31996f06d21977db7746411037
Christopher van Dal <christopher.vandal@domain.com.au>
2017-06-14 13:22:30 +1000

    replaced PaginatedList.cs with PaginatedListExtension.cs

--- Nomad/Extensions/PaginatedListExtension.cs
+++ Nomad/Extensions/PaginatedListExtension.cs
@@ -4,12 +4,12 @@ using System.Linq;
 
 namespace Nomad.Extensions
 {
-    public class PaginatedList<T> : List<T>
+    public class PaginatedListExtension<T> : List<T>
     {
         public int PageIndex { get; private set; }
         public int TotalPages { get; private set; }
 
-        public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
+        public PaginatedListExtension(List<T> items, int count, int pageIndex, int pageSize)
         {
             PageIndex = pageIndex;
             TotalPages = (int)Math.Ceiling(count / (double)pageSize);
@@ -33,12 +33,12 @@ namespace Nomad.Extensions
             }
         }
 
-        public static PaginatedList<T> CreateAsync(List<T> source, int pageIndex, int pageSize)
+        public static PaginatedListExtension<T> CreateAsync(List<T> source, int pageIndex, int pageSize)
         {
             var count = source.Count;
             var items = source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList();
 
-            return new PaginatedList<T>(items, count, pageIndex, pageSize);
+            return new PaginatedListExtension<T>(items, count, pageIndex, pageSize);
         }
     }
 }
