ec81f5545710fb22a662d4883b79066f588f0f01
Christopher van Dal <christopher.vandal@domain.com.au>
2017-08-09 15:45:53 +1000

    initial commit

---
+++ Extensions/AddressFamilyExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+
+namespace Nomad.Extensions
+{
+    public static class AddressFamilyExtension
+    {
+        public static String ConvertToFriendlyAddress(this string clientIp)
+        {
+            IPAddress ipAddress;
+            var address = String.Empty;
+
+            if (IPAddress.TryParse(clientIp, out ipAddress))
+            {
+                switch (ipAddress.AddressFamily)
+                {
+                    case System.Net.Sockets.AddressFamily.InterNetwork:
+                        address = "http://" + clientIp + ":4646";
+                        break;
+                    case System.Net.Sockets.AddressFamily.InterNetworkV6:
+                        address = "http://[" + clientIp + "]:4646";
+                        break;
+                    default:
+                        throw new Exception();
+                }
+            }
+
+            return address;
+        }
+    }
+}
